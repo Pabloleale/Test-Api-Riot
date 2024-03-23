c6010cb0376423e2d29315f2d1207f69228d1d7f
Pabloleale <pleal@utb.edu.co>
2024-03-23 17:48:00 -0500

    nose

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,35 @@
-import React from 'react';
-import { useState} from 'react';
-import axios from 'axios'
+  import React from 'react';
+  import { useState,useEffect} from 'react';
+  import axios from 'axios'
 
 
-function App() {
-  const [data,setData]=useState()
-  const url='https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/pabloleec/RMC?api_key=RGAPI-3ce055d3-35c7-43c0-bdcb-882f165c9589';
+  function App() {
+    const [data,setData]=useState()
+    const url='https://americas.api.riotgames.com/riot/account/v1/accounts/by-riot-id/pabloleec/RMC?api_key=RGAPI-3ce055d3-35c7-43c0-bdcb-882f165c9589';
 
-  axios.get(url)
-  .then(response => {
-    console.log(response.data); 
-    setData(response.data);
-  })
-  .catch(error => {
-    console.error('Error :', error); 
-  });
+    useEffect(()=>{
+      axios.get(url)
+      .then(response => {
+        console.log(response.data); 
+        setData(response.data);
+      })
+      .catch(error => {
+        console.error('Error :', error); 
+      });
 
-  return (
-    <div>
-      <h1>funciona?</h1>
-      {data}
- 
-    </div>
-  );
-}
+    },[])
+    
+    
+    
 
-export default App;
+    return (
+      <div>
+        <h1>funciona?w</h1>
+        {data}
+  
+      </div>
+    );
+  }
+
+  export default App;
 
